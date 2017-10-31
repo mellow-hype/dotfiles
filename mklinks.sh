f64c62480a1a2b40bb01eee47eb7461305415b51
hyperlogic <enrique.castillox@gmail.com>
2017-10-30 23:06:23 -0700

    Simplified script to create symlinks

--- mklinks.sh
+++ mklinks.sh
@@ -5,21 +5,10 @@ I3_PATH="$HOME/.i3/config"
 BASH_PATH="$HOME/.bashrc"
 TMUX_PATH="$HOME/.tmux.conf"
 
-
-if [ -f "$I3_PATH" ]; then
-    mv $I3_PATH "$I3_PATH".bk
-fi
+mv $BASH_PATH $BASH_PATH-bk
+mv $I3_PATH $I3_PATH-bk
+mv $TMUX_PATH $TMUX_PATH-bk
 
 ln -s $WD/i3/config $I3_PATH
-
-if [ -f "$BASH_PATH" ]; then
-    mv $BASH_PATH "$BASH_PATH".bk
-fi
-
 ln -s $WD/bash/bashrc $I3_PATH
-
-if [ -f "$TMUX_PATH" ]; then
-    mv $TMUX_PATH "$TMUX_PATH"
-fi
-
 ln -s $WD/tmux/tmux.conf $TMUX_PATH
