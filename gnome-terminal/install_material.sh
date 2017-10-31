fe928d26408c9a2fbc1f32fd3a8796ce958ee311
hyperlogic <enrique.castillox@gmail.com>
2017-10-30 23:06:53 -0700

    Changed naming label for material theme

--- gnome-terminal/install_material.sh
+++ gnome-terminal/install_material.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Base16 - Gnome Terminal color scheme install script
 
-[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="terminal.sexy"
-[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="terminal-dot-sexy"
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="material"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="material-theme"
 [[ -z "$DCONF" ]] && DCONF=dconf
 [[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
 
